7468efb2f38f381b6446dddcebc7e50950f8eaab
stanislavromanov <stanislav.romanov@outlook.com>
2017-07-25 14:47:29 +0200

    f

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
 export class Wall {
+  private _id: number = new Date().getTime();
+  private _html: string = `
+  <div class="wall-plugin-container wall-plugin-id-${this._id}">
+    <h1>What's Up!?</h1>
+  </div>
+  `;
+
+  constructor(options) {
+    console.log(options);
+    document.body.innerHTML += this._html;
+  }
+
   public sayHello(name: string): void {
     console.log(`Hello ${name}!`);
   }
