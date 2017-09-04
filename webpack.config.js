d8be0e79094f80e97aa1aca28abda90044a14b9f
Kwauka <lutz.kwauka@B0-MCB-043.local>
2017-09-04 14:44:24 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var styleOptions = {};
+styleOptions.use = [
+  {
+    loader: "css-loader",
+    options: {
+      importLoaders: 1
+    }
+  }, 
+  {
+    loader: "sass-loader",
+    options: {
+      outputStyle: 'compressed',
+      sourceMap: false
+    }
+  }
+];
+
+var extractTextPlugin = new ExtractTextPlugin({
+  filename: '[name].css',
+  disable: false,
+  allChunks: true,
+});
+
 module.exports = {
   entry: './src/app.ts',
   output: {
@@ -6,11 +31,19 @@ module.exports = {
     library: 'Wall'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js', '.scss']
   },
   module: {
-    loaders: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(styleOptions),
+      },
     ]
-  }
+  },
+  plugins: [extractTextPlugin]
 }
