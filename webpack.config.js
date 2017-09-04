30928bb1d48b3363759539f4f5e7e1bca6c48d91
stanislavromanov <stanislav.romanov@outlook.com>
2017-09-04 13:55:36 +0200

    remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,10 @@ module.exports = {
     library: 'Wall'
   },
   resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
-    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+    loaders: [
       { test: /\.tsx?$/, loader: 'ts-loader' }
     ]
   }
